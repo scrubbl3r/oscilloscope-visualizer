937cb324d71f59d1fad18996079b35bbe9883d9c
scrubbl3r <79820077+scrubbl3r@users.noreply.github.com>
2026-07-31 17:22:28 -0700

    Add optical rendering A B toggle

--- oscilloscope.js
+++ oscilloscope.js
@@ -376,6 +376,7 @@ var Render =
 		this.outputShader.uContrast = gl.getUniformLocation(this.outputShader, "uContrast");
 		this.outputShader.uBlackPoint = gl.getUniformLocation(this.outputShader, "uBlackPoint");
 		this.outputShader.uGraticuleIntensity = gl.getUniformLocation(this.outputShader, "uGraticuleIntensity");
+		this.outputShader.uOpticalPolish = gl.getUniformLocation(this.outputShader, "uOpticalPolish");
 		this.outputShader.uCanvasAspect = gl.getUniformLocation(this.outputShader, "uCanvasAspect");
 
 		this.texturedShader = this.createShader("texturedVertex","texturedFragment");
@@ -548,6 +549,7 @@ var Render =
 		gl.uniform1f(this.outputShader.uContrast, controls.contrast);
 		gl.uniform1f(this.outputShader.uBlackPoint, controls.blackPoint);
 		gl.uniform1f(this.outputShader.uGraticuleIntensity, controls.graticuleIntensity);
+		gl.uniform1f(this.outputShader.uOpticalPolish, controls.opticalPolish ? 1.0 : 0.0);
 		this.drawTexture(this.lineTexture, this.blur1Texture, this.blur3Texture, this.screenTexture, this.graticuleTexture);
 	},
 
